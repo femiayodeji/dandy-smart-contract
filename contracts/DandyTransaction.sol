5fc67f0eb4cee3b0c9d46f9a1391cf3fa29cb741
Femi Ayodeji <femidotayodeji@gmail.com>
2022-01-25 01:10:41 +0100

    Add Transaction Function

--- contracts/DandyTransaction.sol
+++ contracts/DandyTransaction.sol
@@ -4,7 +4,36 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
  
 contract DandyTransaction {
+    uint256 public transactionCounter;
+
+    struct Transaction{
+        address sender;
+        address receiver;
+        uint amount;
+        string narration;
+        uint256 timestamp;
+    }
+    
+    Transaction[] public transactions;
+
+    event Transfer(address from, address to, uint amount, string narration, uint256 timestamp);
+
     constructor(){
         console.log("Dandy transactions!");
     }
+
+    function SendFund(address receiver, uint amount, string memory narration) public payable {
+        Transaction memory newTransaction = Transaction(msg.sender, receiver, amount, narration, block.timestamp);
+        transactions.push(newTransaction);
+        transactionCounter += 1;
+
+        emit Transfer(
+            newTransaction.sender, 
+            newTransaction.receiver, 
+            newTransaction.amount,
+            newTransaction.narration,
+            newTransaction.timestamp
+        );
+    }
+
 }
